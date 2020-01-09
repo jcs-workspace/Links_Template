378151fb3260fad0b0bda88f5de588746c416bff
Jen-Chieh Shen <jcs090218@gmail.com>
2020-01-09 19:03:11 +0800

    Add all.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * $File: main.js $
+ * $Date: 2020-01-09 18:27:12 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+function main() {
+  document.title = TITLE;
+  document.body.style.backgroundImage = "url('" + BG_IMG + "')";
+}
+main();
