7d1a1cac453f7d2ec5acd14d2b6202e4143c52dd
Jen-Chieh Shen <jcs090218@gmail.com>
2020-01-10 11:59:51 +0800

    Update core logic.

---
+++ js/config.js
@@ -0,0 +1,47 @@
+/**
+ * $File: config.js $
+ * $Date: 2020-01-09 18:27:38 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+const TITLE = 'Links Template Title';
+
+const COPYRIGHT_INFO = 'Copyright © 2020 { INFO }. Built: 2020-01-09.';
+
+const BG_IMG = './images/background.jpg';
+
+var SLOT_1 = {
+  text: 'Slot Text 1',
+  image: './images/frame.png',
+  url: '',
+  visible: true,
+};
+var SLOT_2 = {
+  text: 'Slot Text 2',
+  image: './images/frame.png',
+  url: '',
+  visible: true,
+};
+var SLOT_3 = {
+  text: 'Slot Text 3',
+  image: './images/frame.png',
+  url: '',
+  visible: true,
+};
+var SLOT_4 = {
+  text: 'Slot Text 4',
+  image: './images/frame.png',
+  url: '',
+  visible: true,
+};
+var SLOT_5 = {
+  text: 'Slot Text 5',
+  image: './images/frame.png',
+  url: '',
+  visible: false,
+};
